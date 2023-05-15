d37c68096eee424cd3c088ee02a786f686d9eafa
ArturT <59185635+mr-Arturio@users.noreply.github.com>
2023-05-14 22:07:19 -0400

    working index.js

--- src/index.js
+++ src/index.js
@@ -3,24 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
-  // your code here
+  return <button onClick={props.onClick}>{props.children}</button>;
 };
 
 const Application = () => {
 
-  // your code here
+  const [name, setName] = useState("");
 
   const reset = () => {
     console.log("reset");
-    // your code here
+    document.querySelector("input").value = "";
+    setName("");
   };
 
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder='Type Your Name'
+      />
+      <Button onClick={reset}>
+        Reset
+      </Button>
+      {name && <h1>Hello {name}</h1>}
     </main>
   );
+
 };
 
 ReactDOM.render(<Application />, document.getElementById("root"));
